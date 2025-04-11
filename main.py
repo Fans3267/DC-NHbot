7b4cf5ec2bd6a74019b126aae82808f2effd2858
Fans3267 <fanfan22299999@gmail.com>
2025-04-12 00:30:00 +0800

    reset and hide token

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import re
 from bs4 import BeautifulSoup
 from discord.ext import commands
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+
 client = commands.Bot(command_prefix='!',intents=discord.Intents.all())
 @client.event
 async def on_ready():
@@ -22,14 +28,14 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    match = re.findall(url_regex, message.content)
-    match[0] += "1/"
-    print(match)
+    main_url = re.findall(url_regex, message.content)
+    match = [main_url[0] + "1/"]
 
     if match:
         '''await message.reply(f"{message.author.mention} 連結：{urls[0]}")'''
         await message.add_reaction("🔗")
 
+    #圖片
     try:
         headers = {'User-Agent': 'Mozilla/5.0'}
         res = requests.get(match[0], headers=headers, timeout=5)
@@ -37,16 +43,38 @@ async def on_message(message):
 
         imgs = soup.find_all("img")
         target_image = None
-
         for img in imgs:
             src = img.get("src")
             if src and "galleries" in src:
                 target_image = src
                 break
-        await message.channel.send(target_image)
-    except Exception as e:
-        await message.channel.send("窩很包歉 無法獲取圖片 :<")
-
         
+        all_tags = soup.find_all("span",class_="tags")
+        print(all_tags)
+        for tag_group in all_tags:
+            tag = tag_group.get("href")
+            if tag and "tag/" in tag:
+                await message.channel.send(tag)
+            
+        #發送圖片    
+        await message.channel.send(target_image)
+    except Exception as e: #失敗
+        await message.channel.send("窩很包歉 無法獲取圖片>.<")
 
-client.run('MTM1OTc0MTUxODI2NDQwMTk2Mg.G6ssJ_.lcaRW7P7aMo7J1MMTzRcBZYQ77C5uCAaS4yKOo')
+    #標籤
+    try:
+        headers = {'User-Agent': 'Mozilla/5.0'}
+        res = requests.get(main_url[0], headers=headers, timeout=5)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        
+        all_tags = soup.find_all("span",class_="name")
+        print(all_tags)
+        for tag_group in all_tags:
+            tag = tag_group.get("href")
+            if tag and "tag/" in tag:
+                await message.channel.send(tag)
+            
+    except Exception as e: #失敗
+        await message.channel.send("對噗起 窩無法獲取標籤 :<")
+
+client.run(TOKEN)
