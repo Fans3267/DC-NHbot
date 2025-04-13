1e84e821853a86256a953d4f56e83c1035134ba6
Fans3267 <fanfan22299999@gmail.com>
2025-04-13 15:57:04 +0800

    +error message +title +quick command

--- main.py
+++ main.py
@@ -9,43 +9,56 @@ from discord.ext import commands
 import os
 from dotenv import load_dotenv
 load_dotenv()
-TOKEN = os.getenv("DISCORD_TOKEN")
+TOKEN = os.getenv("BETA_TOKEN")
 
-
-client = commands.Bot(command_prefix='!',intents=discord.Intents.all())
+client = commands.Bot(command_prefix="!",intents=discord.Intents.all())
 @client.event
 async def on_ready():
     print("Running")
 
-'''@client.command()
-async def hello(ctx):
-    await ctx.send("Hello!")'''
-
-url_regex = re.compile(
-    r'https?://nhentai\.net/g/[^\s]+'
-)
-
+#訊息
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
+    await client.process_commands(message)
 
-    #URL處理
+    #辨別網址
+    url_regex = re.compile(
+    r'https?://nhentai\.net/g/[^\s]+'
+    )
     url = re.findall(url_regex, message.content)
+
+    #測試方便用 直接打[n 數字]
+    num_command = re.match(r"n \s*(\w+)", message.content)
+    if num_command:
+        num_command = num_command.group(1)
+        url.append(f"https://nhentai.net/g/{num_command}/")
+        
+    if not url:
+        return
+    
+    #URL處理
     print(url)
     base_url = urlparse(url[0])
     path_parts = [part for part in base_url.path.split('/') if part]
     if len(path_parts) >= 2:
-        # 組合主路徑
+        #去除URL多餘的部分
         base_path = '/'.join(path_parts[:2])
         main_url = f"{base_url.scheme}://{base_url.netloc}/{base_path}/"
 
-        # 取得目標段落
+        #神的語言
         code = path_parts[1]
-
-    print(base_url)
-    print(main_url)
+    
+    #首圖路徑
     pic_url = main_url + "1/"
+
+    #soup獲取html
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    pic_res = requests.get(pic_url, headers=headers, timeout=5)
+    pic_soup = BeautifulSoup(pic_res.text, 'html.parser')
+    main_res = requests.get(main_url, headers=headers, timeout=5)
+    main_soup = BeautifulSoup(main_res.text, 'html.parser')
 
     #反應
     if pic_url:
@@ -54,11 +67,7 @@ async def on_message(message):
 
     #圖片
     try:
-        headers = {'User-Agent': 'Mozilla/5.0'}
-        res = requests.get(pic_url, headers=headers, timeout=5)
-        soup = BeautifulSoup(res.text, 'html.parser')
-
-        imgs = soup.find_all("img")
+        imgs = pic_soup.find_all("img")
         target_image = None
         for img in imgs:
             src = img.get("src")
@@ -66,26 +75,40 @@ async def on_message(message):
                 target_image = src
                 break
     
-    except Exception as e: #失敗
-        await message.channel.send("窩很包歉 無法獲取圖片>.<")
+    except Exception as e:
+        await message.channel.send("窩很包歉 拿不到圖片 >.<")
 
-    #標籤
+    #標題
+    title = None
+    try:
+        title_span = main_soup.find("h2", class_="title")
+        if title_span is None:
+            title_span = main_soup.find("h1")
+            #檢查404
+            if title_span.text.strip().find("404") != -1:
+                await message.remove_reaction("✅", client.user)
+                await message.add_reaction("❌")
+                await message.channel.send(f"{main_url} ❌錯誤連結 >:3 或是已經刪除的作品❌")
+                print("== 錯誤 ==\n")
+                return
+        title_artist = title_span.find("span", class_="before").text.strip()
+        title_name = title_span.find("span", class_="pretty").text.strip()
+        title = f"{title_artist} {title_name}"
+
+    except Exception as e:
+        await message.channel.send("搜哩啦 ;-; 找不到標題")
+
+    # 標籤
     artist = []
     character = []
     language = []
     tag = []
     try:
-        headers = {'User-Agent': 'Mozilla/5.0'}
-        res = requests.get(main_url, headers=headers, timeout=5)
-        soup = BeautifulSoup(res.text, 'html.parser')
-         
-        for all_tags in soup.find_all("span", class_="tags"):
+        for all_tags in main_soup.find_all("span", class_="tags"):
             for tags in all_tags.find_all("a"):
-
                 href = tags.get("href", "")
                 name = tags.find("span", class_="name").text.strip()
                 
-        
                 if "/artist/" in href:
                     artist.append(name)
                 if "/character/" in href:
@@ -95,23 +118,25 @@ async def on_message(message):
                 if "/tag/" in href:
                     tag.append(name)
         
-        await message.delete() #刪除
+        await message.delete() #刪除使用者傳送的訊息
         embed = discord.Embed(
             description=(
-                f"## <{main_url}>\n"
+                f"## {title} \n"
+                f"### N [{code}]({main_url})\n"
                 f"- 由 {message.author.mention} 分享 \n"
                 f"  - 繪師：{', '.join(f'`{a}`' for a in artist)}\n"
                 f"  - 標籤：{', '.join(f'`{t}`' for t in tag)}\n"
                 f"  - 角色：{', '.join(f'`{c}`' for c in character)}\n"
                 f"  - 語言：{', '.join(f'`{l}`' for l in language)}"
             ),
-            color=discord.Color.random()
+            color=discord.Color.random() #亂色
         )
         #Embed
         embed.set_image(url=target_image)
+        print("== 成功 ==\n")
         await message.channel.send(embed=embed)
-    
-    except Exception as e: #失敗
-        await message.channel.send("對噗起 窩無法獲取標籤 :<")
+        
+    except Exception as e:
+        await message.channel.send("對噗起嗚嗚 窩無法獲取標籤")
 
 client.run(TOKEN)
